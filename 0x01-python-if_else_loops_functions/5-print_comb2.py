f8241a9f5a2a8ff4e435167903729ad53f5629bf
yassinerahhou <uassine.rahhou.1@gmail.com>
2023-06-06 08:48:49 -0700

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for y in range(100):
-    print("{:02d}".format(y), end='')
     if y != 99:
-        print(", ", end='')
+        print("{:02d}, ".format(y), end='')
+    else:
+        print("{:02d}".format(y))
