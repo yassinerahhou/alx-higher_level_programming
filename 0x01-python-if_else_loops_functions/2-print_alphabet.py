a1d16dd5d123747e265f0ac338cd5c00ff6a5ec0
yassinerahhou <uassine.rahhou.1@gmail.com>
2023-06-06 07:03:22 -0700

    test

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-alphabet = ""
-for i in range(97, 123):
-alphabet += chr(i)
-print("{}".format(alphabet), end="")
-
+for y in range(97, 123):
+    print("{}".format(chr(b)), end='')
