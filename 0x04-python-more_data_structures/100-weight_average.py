fbd671bfdb1d47c95199d77a3582c500d6381e04
yassinerahhou <uassine.rahhou.1@gmail.com>
2023-06-14 18:26:02 -0700

    end

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+
+    numerator = 0
+    denominator = 0
+    for item in my_list:
+        numerator += item[0] * item[1]
+        denominator += item[1]
+
+    return numerator / denominator
