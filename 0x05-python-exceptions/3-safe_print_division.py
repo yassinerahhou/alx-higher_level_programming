f89d19e17e01d8eeda654aad8a67897fcfc55525
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-06-26 17:55:51 -0700

    x

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return (set_1 & set_2)
+def safe_print_division(a, b):
+    try:
+        x = a / b
+        return x
+    except ZeroDivisionError:
+        x = None
+        return x
+    finally:
+        print("Inside result: {}".format(x))
