93a7d7389472e4470676865c42a0c7a326c4efeb
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-07-10 14:27:50 -0700

    second

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""My fonction"""
+
+
+def is_same_class(obj, a_class):
+    """
+    A function that returns True if the object is exactly a
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
