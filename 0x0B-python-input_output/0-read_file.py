443d2742a851726f97f70676b13ca9bc215a5cf3
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-07-11 12:14:57 -0700

    c

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,9 +7,6 @@ def read_file(filename=""):
     Read and print the contents of a file.
 
     Args:
-        filename (str): The name of the file to be read. Default is an empty string.
-
-    Returns:
         None
     """
     with open(filename, encoding="utf-8") as file:
