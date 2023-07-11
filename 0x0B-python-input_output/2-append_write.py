858f1e23d047681d95f784fca402844882771ff5
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-07-11 12:54:58 -0700

    cc

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""function read from file"""
+"""Function to append text to a file"""
 
-
- def append_write(filename="", text=""):
-         
+def append_write(filename="", text=""):
     """
-    Read and print the contents of a file.
+    Append text to a file and return the number of characters written.
     """
     with open(filename, 'a', encoding="utf-8") as file:
-    return file.write(text)
+        return file.write(text)
