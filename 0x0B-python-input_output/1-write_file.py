395992c04228e16c150b8cef2df0d2428b02ea26
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-07-11 12:43:49 -0700

    xxx

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """
-    Read and print the contents of a file.
-    """
-    with open('filename', encoding="utf-8") as f:
+    """write function"""
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
