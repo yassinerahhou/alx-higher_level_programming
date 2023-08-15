3cbcf6eca6328d51f27411e621c3a2f4f2a43678
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-08-15 16:31:31 -0700

    SQL

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- script that lists all the tables of a database in your MySQL serve
 create table IF NOT EXISTS  first_table ( 
--> id int,
--> name  VARCHAR(256)
+	-> id int,
+	-> name VARCHAR(256)
 -> );
