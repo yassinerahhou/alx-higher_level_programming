49bc7efd2435de531f438219b39a0e5ea59d2dd3
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-08-16 15:15:34 -0700

    SQL

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,4 @@
 -- creat database
-create database if not exists hbtn_0d_usa;
-use hbtn_0d_usa;
 create table if not exists cities (
 id INT not null PRIMARY KEY  AUTO_INCREMENT,
 state_id INT not null PRIMARY KEY FOREIGN KEY (state_id) REFERENCES states(id)  ,
