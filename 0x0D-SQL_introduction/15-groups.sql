d6a822f58165dca60619f47df4cd78e0f2f82a07
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-08-17 10:15:34 -0700

    SQL

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
 -- count
-select score ,
-count(*) as number FROM second_table GROUP BY score ORDER BY score DESC ;
+select score ,count(*) as number FROM second_table GROUP BY score ORDER BY score DESC ;
