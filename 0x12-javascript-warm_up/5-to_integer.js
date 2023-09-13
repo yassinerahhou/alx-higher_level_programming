ab64a2b936c6cfae479a51b2a68384e6bf05c03c
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-13 04:37:33 -0700

    fx5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let myn = process.argv;
-if(isNaN(myn[2])){
-  console.log("Not a number");
+const myn = process.argv;
+if (isNaN(myn[2])) {
+  console.log('Not a number');
 } else {
   console.log('My number: ' + (myn[2]));
 }
