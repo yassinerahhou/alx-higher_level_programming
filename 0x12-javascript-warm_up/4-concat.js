10ff5f96c2a50c96be99f773ddf0c8012d59f91b
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-13 03:54:28 -0700

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
 const myargment = process.argv;
-if (myargment[2] === undefined) {
-  console.log( myargment[2]+' is '+myargment[3]);
-
-}else if (myargment.length === 3) {
-  console.log( myargment[2]+' is '+myargment[3]); 
-}else {
-  console.log(myargment[2]+' is '+myargment[3]);
-}
+console.log(myargment[2]+' is '+myargment[3]);
