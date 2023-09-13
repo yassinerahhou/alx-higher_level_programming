12c56d2a1b4929e5c9b2dd177dffebeeed1f4f41
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-13 05:34:06 -0700

    x

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
-const prc = process.argv;
-if (!isNaN(prc[2])) {
-  let i;
+ const prc = process.argv;
 
-  for (i = 0; i < prc[2]; i++) {
-    console.log('C is fun');
-  }
+if (!isNaN(prc[2])) {
+  funcy(prc);
 } else {
   console.log('Missing number of occurrences');
+}
+function funcy (a){
+  let i;
+
+  for (i = 0; i < a[2]; i++) {
+    console.log('C is fun');
+  }
+
 }
