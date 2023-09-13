6d1b57512950255c759e9f10e1e871b2a0df2c88
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-13 01:51:33 -0700

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const myargment = process.argv.length;
-if (myargment === 2){
+if (myargment === 2) {
   console.log('No argument');
-} else if (myargment === 3){
+} else if (myargment === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
+
