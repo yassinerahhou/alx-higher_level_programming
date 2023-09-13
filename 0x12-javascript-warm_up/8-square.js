beae9d09a2fb6536f947398c3f4b7961f968c20c
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-13 08:35:31 -0700

    square8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const prc = process.argv;
+
+if (!isNaN(prc[2])) {
+  funcy(prc);
+} else {
+  console.log('Missing number of occurrences');
+}
+function funcy (a) {
+  let i;
+  let y;
+  let op = '';
+  for (i = 0; i < a[2]; i++) {
+    for (y = 0; y < a[2]; y++) {
+      op += 'X';
+    }
+
+    console.log(op);
+    op = '';
+  }
+}
