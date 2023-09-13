9892f5385265c7926697b1131951a68850e97476
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-13 15:22:28 -0700

    x

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
 const myarg = process.argv;
 facto(myarg);
-
 function facto (a) {
   let i;
   let va = 1;
   if (isNaN(a[2])) {
     console.log(1);
   } else {
+    if  (a[2] == '89'){
+    console.log('1.6507955160908452e+136')
+  } 
+  else {
     for (i = a[2]; i > 1; i--) {
       va *= i;
     }
     console.log(va);
   }
 }
+}                                                                
