6bcc36925a4a1b9d4d877845f0d7ba898b8c38f6
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-13 00:59:35 -0700

    task00

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const a = "C is fun";
+const b = "Python is cool";
+const c = "JavaScript is amazing";
+console.log(a);
+console.log(b);
+console.log(c);
