c5705eadddadfeb29e77a178a813d668d2d35351
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-15 09:07:02 -0700

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      Rectangle;
+     return;
     } else {
       this.width = w;
       this.height = h;
