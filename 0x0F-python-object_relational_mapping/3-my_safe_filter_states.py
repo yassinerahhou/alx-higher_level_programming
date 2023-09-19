4d4fa25e5a5b73e668d7a3f0840322ce64da3947
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-19 11:48:56 -0700

    fix task3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import mysql.connector
+import sys 
+if __name__ == "__main__":
+    # this block will only execute when the script is run directly.
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+     # name_search = sys.argv[4]
+    # This block establishes a connection to the MySQL database.
+    #  It uses the MySQLdb.connect() method and provides the database connection details, including host, username, password, database name, and port.
+    #  This connection object is stored in the db variable.
+
+conn = MySQLdb.connect(host="localhost", 
+                       port=3306,
+                        user=username ,
+                        passwd=password,
+                        db=db_name         
+                        )
+cur = conn.cursor()
+query_s ="SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+cur.execute(query_s, (argv[4],))
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
