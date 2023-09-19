9a34c73775af674653aa455f75768ac054ee8fa3
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-19 15:03:04 -0700

    try to fix task 05

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""
-    a script that takes in the name of a state as an argument
-    and lists all cities of that state, using the database hbtn_0e_4_usa
-"""
+"""List All states """
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    args = sys.argv
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=args[1],
-                         passwd=args[2],
-                         db=args[3],
-                         charset="utf8"
-                         )
+    ar = sys.argv
+    db = MySQLdb.connect(host='localhost',
+                         user=ar[1],
+                         passwd=ar[2],
+                         db=ar[3],
+                         port=3306)
+
     cur = db.cursor()
-    statement = """SELECT c.name
+    ansr =  """SELECT c.name
                     FROM cities AS c
                     JOIN states AS s
                     ON c.state_id = s.id
                     WHERE BINARY s.name = %s
                     ORDER BY c.id ASC
                 """
-    cur.execute(statement, (args[4],))
+    cur.execute(ansr , (ar[4],))
 
-    query_rows = cur.fetchall()
-    print(', '.join(tuple(row[0] for row in query_rows)))
+    result = cur.fetchall()
+    print(', '.join(tuple(row[0] for row in result)))
+    # for city in result:
+    #     print(city)
+
+    # cur.close()
