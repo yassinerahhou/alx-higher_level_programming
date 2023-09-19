7d2602b3a706aebcbb49e0e6915f9a5d030d7051
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-19 12:00:37 -0700

    fix task3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+"""List All states inside hbtn_0e_0_usa db """
 
 import MySQLdb
-import sys 
+import sys
 from sys import argv
+
 if __name__ == "__main__":
-    # this block will only execute when the script is run directly.
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-     # name_search = sys.argv[4]
-    # This block establishes a connection to the MySQL database.
-    #  It uses the MySQLdb.connect() method and provides the database connection details, including host, username, password, database name, and port.
-    #  This connection object is stored in the db variable.
+    # name_search = sys.argv[4]
 
-conn = MySQLdb.connect(host="localhost", 
-                       port=3306,
-                        user=username ,
-                        passwd=password,
-                        db=db_name         
-                        )
-cur = conn.cursor()
-query_s ="SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-cur.execute(query_s, (argv[4],))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    db = MySQLdb.connect(host='localhost',
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         port=3306)
+
+    cur = db.cursor()
+    query_s = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query_s, (argv[4],))
+
+    result = cur.fetchall()
+    for sta in result:
+        print(sta)
+
+    cur.close()
+    db.close()
