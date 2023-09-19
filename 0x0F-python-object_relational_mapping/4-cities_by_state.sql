2ce595577a2aed3a80709ace4aa0189172b8f914
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-19 12:29:47 -0700

    task4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.sql
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.sql
@@ -3,22 +3,20 @@
 
 import MySQLdb
 import sys
-from sys import argv
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+
+def myfun():
+    ar = sys.argv
 
     db = MySQLdb.connect(host='localhost',
-                         user=username,
-                         passwd=password,
-                         db=db_name,
+                         user=ar[1],
+                         passwd=ar[2],
+                         db=ar[3],
                          port=3306)
 
     cur = db.cursor()
-    cur.execute(("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id"""))
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
 
     result = cur.fetchall()
     for city in result:
@@ -26,3 +24,5 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
+    if __name__ == "__main__":
+        myfun()
