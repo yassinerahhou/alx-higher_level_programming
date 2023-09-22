aee3a0d04b2befc12924873fa7da915f383a61f6
yassine rahhou <125989457+yassinerahhou@users.noreply.github.com>
2023-09-22 12:33:57 +0100

    ame

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-""" 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessio State object with the name passed as argument from the database
+""" prints the State object with the name passed as argument from the database
 """
 import sys
 from model_state import Base, State
