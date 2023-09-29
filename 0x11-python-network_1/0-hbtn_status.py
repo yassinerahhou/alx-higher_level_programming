93395ff232bc26c7532595368e4baa772c4ad1de
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-29 16:00:41 -0700

    Python script that fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 import urllib.request
 url = "https://alx-intranet.hbtn.io/status"
@@ -13,20 +12,3 @@ with urllib.request.urlopen(url) as response:
     decoded_data = content.decode("utf-8")
     print('\t- utf8 content: ',decoded_data)
     # print(' - status code: ',response.getcode()) # 200
-=======
-#!/usr/bin/python3
-import urllib.request
-url = "https://alx-intranet.hbtn.io/status"
-
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    print('Body response:')
-    print('\t- content: ',content)
-    print('\t- type: ',type(content))
-    # response = urllib.request.urlopen(url)
-    # data = response.read()
-    decoded_data = content.decode("utf-8")
-    print('\t- utf8 content: ',decoded_data)
-    # print(' - status code: ',response.getcode()) # 200
-   
->>>>>>> 66174f566d2025a37ad5185dedf9720f9a54a2cd
