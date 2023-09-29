d832dd7627f9cc11cf46fe86c4282874114e1ee2
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-09-29 16:04:21 -0700

    Python script that fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,4 @@ with urllib.request.urlopen(url) as response:
     print('Body response:')
     print('\t- content: ',content)
     print('\t- type: ',type(content))
-    # response = urllib.request.urlopen(url)
-    # data = response.read()
-    decoded_data = content.decode("utf-8")
     print('\t- utf8 content: ',decoded_data)
-    # print(' - status code: ',response.getcode()) # 200
