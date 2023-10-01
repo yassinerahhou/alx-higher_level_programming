611f44f352e99c841291b7fa7cfd09887afa5bef
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-10-01 10:00:25 -0700

    displays the value of the X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,10 @@ import urllib.request
 import sys
 arg = sys.argv[1]
 url = "https://alx-intranet.hbtn.io"
-repons = urllib.request.urlopen(arg) 
-print(repons.getheader("X-Request-Id"))
+with urllib.request.urlopen(arg) as repons :
+# repons = urllib.request.urlopen(arg) 
+
+    print(repons.getheader("X-Request-Id"))
 
 if __name__ == "__main__":
     pass
