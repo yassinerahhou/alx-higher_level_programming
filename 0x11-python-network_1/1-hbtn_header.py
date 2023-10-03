e70b5f3e34f60e178349d0a025ebf56350668c9d
yassine rahhou <125989457+yassinerahhou@users.noreply.github.com>
2023-10-03 01:11:12 +0100

    Error code #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-#Python script that takes in a URL, sends a request to t
-import urllib
-import urllib.request
+
 import sys
+import urllib.request
+
 if __name__ == "__main__":
-    arg = sys.argv[1]
-    
-    with urllib.request.urlopen(arg) as response:
-        x_request_id = response.getheader("X-Request-Id")
-        print(x_request_id)
+
+    with urllib.request.urlopen(sys.argv[1]) as repons:
+        body = repons.info()
+        d = 'X-Request-Id'
+        if d in body:
+            st = body[d]
+            print(st)
