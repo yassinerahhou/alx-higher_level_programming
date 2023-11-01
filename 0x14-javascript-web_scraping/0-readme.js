09db72e4008577da433b804c8d3dfcf8fc601b0e
Yassine rahhou <yassine.rahhou@gmail.com>
2023-11-01 17:56:52 +0000

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// const filep = process.argv[2];
+fs = require('fs');
+const file = fs.readFileSync(process.argv[2], 'utf8',(err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
+console.log(file)
