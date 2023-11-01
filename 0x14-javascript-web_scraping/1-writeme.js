d374383ed9e892939f394ab80adeeda36a89080a
Yassine rahhou <yassine.rahhou@gmail.com>
2023-11-01 18:38:52 +0000

    writefilee

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,10 @@
 const filep = process.argv[2];
 const fs = require('fs');
 
-const file = fs.writeFileSync(filep, argv[3],'utf8',(err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(data);
-    }
-  });
+fs.writeFileSync(filep, process.argv[3], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
