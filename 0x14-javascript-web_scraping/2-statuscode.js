97b2101ce857de07642717d1bafddd56761141a8
Yassine rahhou <yassine.rahhou@gmail.com>
2023-11-01 18:59:09 +0000

    statu code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
 // const fs = require('fs');
-const request = require('request')
-let url = process.argv[2];
+const request = require('request');
+const url = process.argv[2];
 
 // Request URL
 // let url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-request(url, (error, response, body) => {
-	// Printing the error if occurred
-	if (error) console.log(error)
+request(url, (error, response) => {
+  // Printing the error if occurred
+  if (error) console.log(error);
 
-	// Printing status code
-	console.log('code:',response.statusCode);
+  // Printing status code
+  console.log('code:', response.statusCode);
 
-	// Printing body
-	// console.log(body);
+  // Printing body
+  // console.log(body);
 });
