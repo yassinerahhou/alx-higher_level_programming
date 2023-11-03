c4c8fb0df3bdc9f658b2eca1fb6a57f5b79acf12
Yassine rahhou <yassine.rahhou@gmail.com>
2023-11-03 13:59:42 +0000

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // DOMContentLoaded : we use it when  we need to make sure that our js code dosen't run until the entire HTML document has been loaded
+  document.querySelector("header").style.color = "#FF0000";
+});
